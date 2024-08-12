64f95537bbd7389f19757c42c1f8b8f276e09a3e
Leonid Lugovoj <yotohana.dev@gmail.com>
2024-08-12 19:37:38 +0500

    - Added DataBase PostgreSQL - Added password hashing - In the development of the JWT token

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module api-certificates
 
 go 1.22.1
+
+require github.com/jackc/pgx v3.6.2+incompatible
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/jackc/pgx/v5"
 )
 
 type Data struct {
-	Login string `json:"login"`
+	Login    string `json:"login"`
 	Password string `json:"password"`
 }
 
 type Response struct {
-	Code int `json:"code"`
+	Code    int    `json:"code"`
 	Message string `json:"message"`
 }
 
@@ -20,7 +26,13 @@ type Token struct {
 	Token string `json:"token"`
 }
 
-func LoginHandler(w http.ResponseWriter, r *http.Request){
+type MyClaims struct {
+	jwt.RegisteredClaims
+	Id int `json:"id"`
+	Login string `json:"login"`
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
@@ -33,8 +45,46 @@ func LoginHandler(w http.ResponseWriter, r *http.Request){
 		return
 	}
 
-	if data.Login == "admin" && data.Password == "correct_password" {
-		resp := Token{Token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFeHAiOiIyMDIzLTEyLTE4VDEyOjI5OjE5LjEwNjg0MTQzOVoiLCJVc2VyTG9naW4iOiJhZG1pbiJ9.0Dvg7vFTrdSX2F4751ae6Id9weC5ATvF1sQPuvejiFE"}
+	conn, err := pgx.Connect(context.Background(),"postgres://adminfront:123@localhost:5432/adminfront")
+	if err != nil {
+		fmt.Fprintf(os.Stderr,"Unable to connect to database : %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var password_match bool
+	err = conn.QueryRow(context.Background(),"SELECT (password = crypt($1, password)) as password_match FROM users WHERE login = $2", data.Password, data.Login).Scan(&password_match)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	log.Printf("Password_match: %v", password_match)
+
+	if password_match {
+		var id int
+		err = conn.QueryRow(context.Background(),"SELECT id FROM users WHERE login = $1", data.Login).Scan(&id)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+			os.Exit(1)
+		}
+
+		claims := MyClaims{
+			RegisteredClaims: jwt.RegisteredClaims{},
+			Login: data.Login,
+			Id: id,
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		var mySigningKey = []byte("secret-key")
+
+		StrToken, err := token.SignedString(mySigningKey)
+		if err != nil {
+			log.Fatalf("Произошла ошибка %v\n", err)
+		}
+
+		resp := Token{Token: StrToken}
+		log.Println("Login data is correct.")
 		jsonData, err := json.Marshal(resp)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -44,28 +94,28 @@ func LoginHandler(w http.ResponseWriter, r *http.Request){
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(jsonData)
 	} else {
-		resp := Response{Code: 401, Message: "Unauthorized"}
-		jsonData, err := json.Marshal(resp)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(jsonData)
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		log.Println("Login data is not correct. Error!")
 	}
 }
 
-func CheckHandler(w http.ResponseWriter, r *http.Request)  {
+func CheckHandler(w http.ResponseWriter, r *http.Request) {
 	token := r.Header.Get("Authorization")
-	if token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFeHAiOiIyMDIzLTEyLTE4VDEyOjI5OjE5LjEwNjg0MTQzOVoiLCJVc2VyTG9naW4iOiJhZG1pbiJ9.0Dvg7vFTrdSX2F4751ae6Id9weC5ATvF1sQPuvejiFE" {
+	if token == StrToken{
 		w.WriteHeader(http.StatusNoContent)
 	}
+	log.Println("Authorization token is sending...")
 }
 
+// func GrantsHandler(w http.ResponseWriter, r *http.Request)  {
+// 	header := r.FormValue("page")
+// 	log.Printf("%s Page", header)
+// }
+
 func main() {
- http.HandleFunc("/admin/api/v1/auth/login", LoginHandler)
- http.HandleFunc("/admin/api/v1/auth/check", CheckHandler)
- log.Println("Server started at localhost:80")
- http.ListenAndServe(":80", nil)
+	http.HandleFunc("/login", LoginHandler)
+	http.HandleFunc("/check", CheckHandler)
+	// http.HandleFunc("/grants", GrantsHandler)
+	log.Println("Server started at localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
