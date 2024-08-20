f593d0222954fb24fd51b6125848f99b11a9aec2
Leonid Lugovoj <yotohana.dev@gmail.com>
2024-08-21 01:34:46 +0500

    added handler for grants

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"strings"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/jackc/pgx/v5"
@@ -32,7 +31,27 @@ type MyClaims struct {
 	Login string `json:"login"`
 }
 
+type DataGrants struct {
+	Grants []Grant `json:"grants"`
+}
+
+type Grant struct {
+	ID           int          `json:"id" db:"id"`
+	Title        string       `json:"title" db:"title"`
+	SourceURL    string       `json:"source_url" db:"source_url"`
+	FilterValues FilterValues `json:"filter_values"`
+}
+
+type FilterValues struct {
+	CuttingOffCriteria []int `json:"cutting_off_criteria" db:"cutting_off_criterea"`
+	ProjectDirection   []int `json:"project_direction" db:"project_directions"`
+	Amount             int   `json:"amount" db:"amount"`
+	LegalForm          []int `json:"legal_form" db:"legal_forms"`
+	Age                int   `json:"age" db:"age"`
+}
+
 var mySigningKey = []byte("secret-key")
+var ctx = context.Background()
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
@@ -47,25 +66,23 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	conn, err := pgx.Connect(context.Background(),"postgres://adminfront:123@localhost:5432/adminfront")
+	conn, err := pgx.Connect(ctx,"postgres://adminfront:123@localhost:5432/adminfront")
 	if err != nil {
 		log.Fatalf("Unable to connect to database : %v\n", err)
-		os.Exit(1)
 	}
-	defer conn.Close(context.Background())
+	defer conn.Close(ctx)
 
 	var password_match bool
-	err = conn.QueryRow(context.Background(),"SELECT (password = crypt($1, password)) as password_match FROM users WHERE login = $2", data.Password, data.Login).Scan(&password_match)
+	err = conn.QueryRow(ctx,"SELECT (password = crypt($1, password)) as password_match FROM users WHERE login = $2", data.Password, data.Login).Scan(&password_match)
 	if err != nil {
 		log.Fatalf("QueryRow failed: %v\n", err)
-		os.Exit(1)
 	}
 
 	log.Printf("Password_match: %v", password_match)
 
 	if password_match {
 		var id int
-		err = conn.QueryRow(context.Background(),"SELECT id FROM users WHERE login = $1", data.Login).Scan(&id)
+		err = conn.QueryRow(ctx,"SELECT id FROM users WHERE login = $1", data.Login).Scan(&id)
 		if err != nil {
 			log.Fatalf("QueryRow failed: %v\n", err)
 		}
@@ -99,6 +116,11 @@ func Login(w http.ResponseWriter, r *http.Request) {
 }
 
 func Check(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	
 	token := r.Header.Get("Authorization")
 	token = strings.TrimPrefix(token, "Bearer ")
 	
@@ -112,7 +134,7 @@ func Check(w http.ResponseWriter, r *http.Request) {
 	claims := &MyClaims{}
 	parsedToken, err := jwt.ParseWithClaims(token, claims, keyFunc)
 	if err != nil {
-		log.Fatalf("Ошибка разбора: %v", err)
+		log.Fatalf("Ошибка разбора check: %v", err)
 	}
 
 	if !parsedToken.Valid {
@@ -123,5 +145,50 @@ func Check(w http.ResponseWriter, r *http.Request) {
 }
 
 func Grants (w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	
+	conn, err := pgx.Connect(ctx,"postgres://adminfront:123@localhost:5432/adminfront")
+	if err != nil {
+		log.Fatalf("Unable to connect to database : %v\n", err)
+	}
+	defer conn.Close(ctx)
+
+	query := "SELECT id, title, source_url, project_directions, amount, legal_forms, age, cutting_off_criterea FROM grants WHERE id = $1"
+    var grant Grant
+
+    // Выполнение запроса с привязкой параметра id
+    err = conn.QueryRow(context.Background(), query, 1).Scan(
+        &grant.ID,
+        &grant.Title,
+        &grant.SourceURL,
+        // Здесь нужно будет преобразовать или десериализовать значения, если они JSON
+        &grant.FilterValues.ProjectDirection, // предполагаем, что это массив
+        &grant.FilterValues.Amount,
+        &grant.FilterValues.LegalForm, // если это массив
+        &grant.FilterValues.Age,
+        &grant.FilterValues.CuttingOffCriteria, // если это массив
+    )
+    
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Создание объекта DataGrants
+    dataGrants := DataGrants{
+        Grants: []Grant{grant},
+    }
+
+    // Преобразование результата в JSON
+    jsonResult, err := json.Marshal(dataGrants)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Вывод JSON
+    fmt.Println(string(jsonResult))
+
 	
 }
